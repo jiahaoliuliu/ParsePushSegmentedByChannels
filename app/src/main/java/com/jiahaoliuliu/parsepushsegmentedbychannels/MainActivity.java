10605d410e1241108d7fd7862e8918ca3b312bc0
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-06-11 16:21:56 +0400

    Get the list of channels

--- app/src/main/java/com/jiahaoliuliu/parsepushsegmentedbychannels/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/parsepushsegmentedbychannels/MainActivity.java
@@ -2,12 +2,25 @@ package com.jiahaoliuliu.parsepushsegmentedbychannels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseInstallation;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get subscribed channels
+        List<String> subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
+        for (String channel : subscribedChannels) {
+            Log.v(TAG, "Channel: " + channel);
+        }
     }
 }
